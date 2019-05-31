05b7cf9043c80f18e98cc519084e50e209dec32e
EvanGrace <evthepenguin@gmail.com>
2019-05-31 06:45:13 -0700

    Update browserapp.js
    Trying different UUIDs to match a default sketch on the ESP32. (Gatt_Client)

--- browserapp.js
+++ browserapp.js
@@ -5,15 +5,16 @@ $(function(){
 		name: ["MYDEVICE"]
 		}
 	],
-	optionalServices: ["4fafc201-1fb5-459e-8fcc-c5c9c331914b"]
+	optionalServices: ["00FF"] //Old one: 4fafc201-1fb5-459e-8fcc-c5c9c331914b
 	};
+	//Old Characteristic: beb5483e-36e1-4688-b7f5-ea07361b26a8
 
 	$("#test").click(() => {
 		console.log("Running BLE Code");
 		navigator.bluetooth.requestDevice(requestDeviceParms).then(device => {
 			device.gatt.connect().then(gattServer =>{
-				gattServer.getPrimaryService("4fafc201-1fb5-459e-8fcc-c5c9c331914b").then(gattService=>{
-					gattService.getCharacteristic("beb5483e-36e1-4688-b7f5-ea07361b26a8").then(gattCharacteristic=>{
+				gattServer.getPrimaryService("00FF").then(gattService=>{
+					gattService.getCharacteristic("FF01").then(gattCharacteristic=>{
 						gattCharacteristic.startNotifications().then(gattCharacteristic=>{
 							gattCharacteristic.addEventListener("characteristicvaluechanged", event=>{
 								var value = event.target.value.getUint8(0);
