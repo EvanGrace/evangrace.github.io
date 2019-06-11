e324f7eea7dd7844dda0d8f367d37ff932f6999b
EvanGrace <evthepenguin@gmail.com>
2019-06-11 14:58:21 -0600

    Update browserapp.js
    Formatting

--- browserapp.js
+++ browserapp.js
@@ -10,11 +10,16 @@ $(function(){
 
 	$("#connect").click(() => {
 		console.log("Running BLE Code");
-		navigator.bluetooth.requestDevice(requestDeviceParms).then(device => {
-			device.gatt.connect().then(gattServer =>{
-				gattServer.getPrimaryService("2937aa48-6f5c-4778-9078-f804143a1c71").then(gattService=>{
-					gattService.getCharacteristic("58eabb08-ea2d-4606-a3ec-f6adefbf7cb2").then(gattCharacteristic=>{
-						gattCharacteristic.startNotifications().then(gattCharacteristic=>{
+		navigator.bluetooth.requestDevice(requestDeviceParms)
+			.then(device => {
+			device.gatt.connect()
+				.then(gattServer =>{
+				gattServer.getPrimaryService("2937aa48-6f5c-4778-9078-f804143a1c71")
+					.then(gattService=>{
+					gattService.getCharacteristic("58eabb08-ea2d-4606-a3ec-f6adefbf7cb2")
+						.then(gattCharacteristic=>{
+						gattCharacteristic.startNotifications()
+							.then(gattCharacteristic=>{
 							gattCharacteristic.addEventListener("characteristicvaluechanged", event=>{
 								var value = event.target.value.getUint8(0);
 								$("#notifiedValue").text("" + value);
